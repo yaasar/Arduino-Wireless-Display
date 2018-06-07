dcf15ed94cf12b4f718cdc162a246576b32b5cec
yaasar <31248284+yaasar@users.noreply.github.com>
2018-06-07 20:29:05 +0530

    Add files via upload

---
+++ bluetooth_to_lcd.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+  lcd.begin();
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    delay(100);
+    lcd.clear();
+    while (Serial.available() > 0)
+    {
+      lcd.write(Serial.read());
+
+    }
+  }
+}
